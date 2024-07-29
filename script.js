63614e0054d5f56a545fdecdb85bf6dfe39ce290
io-start-trezr <ajaychourasia3096@gmail.com>
2024-07-29 15:59:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const bingApiKey = 'YOUR_BING_API_KEY';
+
+function search() {
+    const query = document.getElementById('search-query').value;
+    searchGoogle(query);
+    searchBing(query);
+}
+
+function searchBing(query) {
+    const url = `https://api.bing.microsoft.com/v7.0/search?q=${query}&count=10`;
+
+    fetch(url, {
+        headers: { 'Ocp-Apim-Subscription-Key': bingApiKey }
+    })
+    .then(response => response.json())
+    .then(data => {
+        displayResults(data.webPages.value, 'Bing');
+    })
+    .catch(error => console.error('Error:', error));
+}
